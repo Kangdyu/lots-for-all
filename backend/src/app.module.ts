77eb26ed3e314e874e5aa3103519da30f2b241c0
kweonminsung <kevin6528@scg.skku.ac.kr>
2022-06-28 21:50:07 +0900

    feature(backend) login api & get user info

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from "./users/users.module";
-const config = require("../ormconfig")[process.env.NODE_ENV || "development"];
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+const config = require('../ormconfig')[process.env.NODE_ENV || 'development'];
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config["db"]), UsersModule],
+  imports: [TypeOrmModule.forRoot(config['db']), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsOptional()
+  imageUrl: string;
+}

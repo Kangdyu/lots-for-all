9393ff7297b496a92fdfe5fab62aa0df3609489c
kweonminsung <kevin6528@scg.skku.ac.kr>
2022-06-28 13:45:44 +0900

    feat(backend): env & DB settings

---
+++ backend/src/users/entities/users.entitiy.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: "255", unique: true, nullable: false })
+  username: string;
+
+  @Column({ type: "varchar", length: "255", nullable: false })
+  password: string;
+
+  @Column({ type: "varchar", length: "255" })
+  imageUrl: string;
+}

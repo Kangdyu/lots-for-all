302b526d1254c27b74b89fc92d11f12d43087d4f
Kangdyu <kangdyu99@gmail.com>
2022-06-28 19:56:27 +0900

    feat(frontend): add shadow on GameCard

--- frontend/src/components/common/GameCard/styles.tsx
+++ frontend/src/components/common/GameCard/styles.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { hexToRgba } from "utils/color";
 
 export const GameCardContainer = styled.div`
   display: flex;
@@ -14,6 +15,7 @@ export const GameCardContainer = styled.div`
   overflow: hidden;
 
   background-color: ${(props) => props.theme.color.white};
+  box-shadow: 0 6px 16px 0 ${(props) => hexToRgba(props.theme.color.primary700, 0.1)};
 `;
 
 export const GameCardName = styled.span`

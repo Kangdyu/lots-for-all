d5ed19a385b83afc938354301af2c284324487c2
Kangdyu <kangdyu99@gmail.com>
2022-06-28 21:19:27 +0900

    feat(frontend): GameSection default layout

--- frontend/src/components/pages/pick/GameSection/index.tsx
+++ frontend/src/components/pages/pick/GameSection/index.tsx
@@ -2,7 +2,13 @@ import { GameRoute } from "constants/games";
 import { GAME_ROUTES } from "constants/routes";
 import Link from "next/link";
 import { HTMLAttributes } from "react";
-import { GameNavigation, GameNavigationAnchor } from "./styles";
+import {
+  GameForm,
+  GameFormContainer,
+  GameNavigation,
+  GameNavigationAnchor,
+  GameParticipants,
+} from "./styles";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   game: GameRoute;
@@ -20,6 +26,14 @@ function GameSecton({ game, ...props }: Props) {
           </Link>
         ))}
       </GameNavigation>
+      <GameFormContainer>
+        <GameForm>
+          <h2>게임 설정</h2>
+        </GameForm>
+        <GameParticipants>
+          <h2>인원</h2>
+        </GameParticipants>
+      </GameFormContainer>
     </section>
   );
 }

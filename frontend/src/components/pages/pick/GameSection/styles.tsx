73ce63d0e9ddaa80626d2f6e76e4549cc27200a8
Kangdyu <kangdyu99@gmail.com>
2022-06-29 09:43:51 +0900

    feat(frontend): default game setting form layout

--- frontend/src/components/pages/pick/GameSection/styles.tsx
+++ frontend/src/components/pages/pick/GameSection/styles.tsx
@@ -1,37 +1,15 @@
-import { css, Theme } from "@emotion/react";
 import styled from "@emotion/styled";
-import { hexToRgba } from "utils/color";
+import Input from "components/common/Input";
 
-export const GameFormContainer = styled.div`
+export const Flex = styled.div`
   display: flex;
-  justify-content: center;
+  align-items: flex-end;
 `;
 
-const boxStyle = (theme: Theme) => css`
-  height: 500px;
-  padding: 24px;
-
-  border-radius: 24px;
-  background-color: ${theme.color.white};
-  box-shadow: 0 6px 16px 0 ${hexToRgba(theme.color.primary700, 0.1)};
-
-  h2 {
-    font-size: 20px;
-    font-weight: 400;
-  }
+export const FlexColumn = styled(Flex)`
+  flex-direction: column;
 `;
 
-export const GameForm = styled.form`
-  ${(props) => boxStyle(props.theme)};
-
-  max-width: 500px;
-  width: 100%;
-  margin-right: 30px;
-`;
-
-export const GameParticipants = styled.ul`
-  ${(props) => boxStyle(props.theme)};
-
-  max-width: 200px;
-  width: 100%;
+export const StyledInput = styled(Input)`
+  margin-right: 8px;
 `;

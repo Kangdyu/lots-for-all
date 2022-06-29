48ccb8fe66d2df78f5ae71e0de110a0070853e6f
Kangdyu <kangdyu99@gmail.com>
2022-06-29 10:42:37 +0900

    feat(frontend): form을 NameListForm으로 분리, NameTag container styling

--- frontend/src/components/pages/pick/GameSection/index.tsx
+++ frontend/src/components/pages/pick/GameSection/index.tsx
@@ -2,8 +2,7 @@ import Button from "components/common/Button";
 import SectionTitle from "components/common/SectionTitle";
 import { GameRoute } from "constants/games";
 import { FormEvent, HTMLAttributes, useCallback, useRef, useState } from "react";
-import NameTag from "../NameTag";
-import { Flex, StyledInput } from "./styles";
+import { Flex, NameListContainer, NameListForm, StyledInput, StyledNameTag } from "./styles";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   game: GameRoute;
@@ -18,6 +17,7 @@ function GameSecton({ game, ...props }: Props) {
 
     if (nameInputRef.current) {
       const name = nameInputRef.current.value;
+      if (name === "") return;
       setList((p) => p.concat(name));
       nameInputRef.current.value = "";
     }
@@ -25,33 +25,35 @@ function GameSecton({ game, ...props }: Props) {
 
   return (
     <section {...props}>
-      <SectionTitle css={{ marginBottom: "24px" }}>기본 설정</SectionTitle>
-      <form onSubmit={handleListSubmit}>
-        <Flex css={{ justifyContent: "space-between", marginBottom: "40px" }}>
-          <Flex>
-            <StyledInput type="text" label="제목" placeholder="게임 제목 입력" />
-            <StyledInput
-              type="text"
-              label="인원 / 항목"
-              placeholder="추가할 인원 / 항목 입력"
-              ref={nameInputRef}
-            />
-            <Button>명단</Button>
-          </Flex>
-          <Flex>
-            <Button css={{ marginRight: "8px" }}>프리셋</Button>
-            <Button>최근 플레이</Button>
-          </Flex>
-        </Flex>
-        <SectionTitle css={{ marginBottom: "12px" }}>참여 인원</SectionTitle>
+      <Flex css={{ justifyContent: "space-between", marginBottom: "24px" }}>
+        <SectionTitle>기본 설정</SectionTitle>
         <Flex>
-          {list.map((item, idx) => (
-            <NameTag key={idx} css={{ marginRight: "8px" }}>
-              {item}
-            </NameTag>
-          ))}
+          <Button css={{ marginRight: "8px" }}>프리셋</Button>
+          <Button>최근 플레이</Button>
         </Flex>
-      </form>
+      </Flex>
+      <Flex css={{ justifyContent: "space-between", marginBottom: "40px" }}>
+        <NameListForm onSubmit={handleListSubmit}>
+          <StyledInput type="text" label="제목" placeholder="게임 제목 입력" />
+          <StyledInput
+            type="text"
+            label="인원 / 항목"
+            placeholder="추가할 인원 / 항목 입력"
+            ref={nameInputRef}
+          />
+          <Button type="submit" css={{ marginRight: "8px" }}>
+            추가
+          </Button>
+          <Button type="button">명단에서 추가</Button>
+        </NameListForm>
+      </Flex>
+
+      <SectionTitle css={{ marginBottom: "24px" }}>참여 인원</SectionTitle>
+      <NameListContainer>
+        {list.map((item, idx) => (
+          <StyledNameTag key={idx}>{item}</StyledNameTag>
+        ))}
+      </NameListContainer>
     </section>
   );
 }
--- frontend/src/components/pages/pick/GameSection/styles.tsx
+++ frontend/src/components/pages/pick/GameSection/styles.tsx
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled";
 import Input from "components/common/Input";
+import NameTag from "../NameTag";
 
 export const Flex = styled.div`
   display: flex;
-  align-items: flex-end;
 `;
 
 export const FlexColumn = styled(Flex)`
@@ -12,4 +12,18 @@ export const FlexColumn = styled(Flex)`
 
 export const StyledInput = styled(Input)`
   margin-right: 8px;
+`;
+
+export const NameListForm = styled.form`
+  display: flex;
+`;
+
+export const NameListContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 8px 0 0 8px;
+`;
+
+export const StyledNameTag = styled(NameTag)`
+  margin: -8px 16px 16px -8px;
 `;

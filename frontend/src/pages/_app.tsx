fc67c84ffbe636f0c2da70d81bd429fb334f8eb5
Kangdyu <kangdyu99@gmail.com>
2022-06-30 22:37:04 +0900

    feat(frontend): 사이트 기본 타이틀

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,16 +4,22 @@ import GlobalStyles from "styles/GlobalStyles";
 import { theme } from "styles/theme";
 import axios from "axios";
 import { SERVER_URL } from "constants/endpoints";
+import Head from "next/head";
 
 axios.defaults.baseURL = SERVER_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>모두의 추첨</title>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 

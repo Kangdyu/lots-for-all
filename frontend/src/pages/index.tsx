edcd9ecfdcda8dd9cad3c60f75b771f58b9a9752
Kangdyu <kangdyu99@gmail.com>
2022-06-30 20:43:06 +0900

    fix(frontend): key error

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -50,7 +50,7 @@ function IndexPage() {
       <Layout>모두의 추첨</Layout>
       {histories?.map((gameHistory: GameHistory) => (
         <HistoryCard
-          key={gameHistory.id}
+          key={`${gameHistory.type}/${gameHistory.id}`}
           title={gameHistory.title}
           gameType={gameHistory.type}
           numPeople={gameHistory.number}
